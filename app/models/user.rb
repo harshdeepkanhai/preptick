aaa13382af6f4335aed8130992fbfd3abd1e9e15
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-06-21 13:19:59 +0530

    Add Validations for image and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :image, presence: true, uniqueness: true
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
